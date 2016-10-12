6303ca3192ae30bc00031576887659549304d92c
BigHairBand-PC\BigHairBand <adororis@gmail.com>
2016-10-11 21:51:02 -0600

    got images displaying

--- src/main/java/com/app/GmaDiariesApplication.java
+++ src/main/java/com/app/GmaDiariesApplication.java
@@ -1,7 +1,17 @@
 package com.app;
 
+import java.awt.image.BufferedImage;
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,11 +22,34 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GmaDiariesApplication {
 	
-	@RequestMapping("/resource")
-	public Map<String, Object> home() {
+	@RequestMapping(value="/resource", produces = {"application/json", "image/jpeg"})
+	public Map<String, Object> home(HttpServletRequest request) {
+		
+		
+		//handle image reading, encoding, writing
+		ByteArrayOutputStream jpegOutputStream = new ByteArrayOutputStream();
+		BufferedImage image = null;
+		
+		try {
+			InputStream is;
+			is = new BufferedInputStream(
+		          new FileInputStream("src/main/resources/static/images/1941Postcard.jpg"));
+			image = ImageIO.read(is);
+			ImageIO.write(image, "jpeg", jpegOutputStream);			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {	
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.getMessage();
+		}
+
+		byte[] gmaImageByte = jpegOutputStream.toByteArray();
+		
 		Map<String, Object> model = new HashMap<String, Object>();
 		model.put("date", "12/12/1945");
-		model.put("image", "Hello World");
+		model.put("image", gmaImageByte);
 		return model;
 	}
 	
