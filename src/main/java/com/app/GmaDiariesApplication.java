d81cfa0fcc31553da4141788ff332eb1cdfdcf7c
BigHairBand-PC\BigHairBand <adororis@gmail.com>
2016-10-15 23:15:28 -0600

    Basic Spring Security Implementation

--- src/main/java/com/app/GmaDiariesApplication.java
+++ src/main/java/com/app/GmaDiariesApplication.java
@@ -7,16 +7,32 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.security.Principal;
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.imageio.ImageIO;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.WebUtils;
 
 @SpringBootApplication
 @RestController
@@ -53,8 +69,48 @@ public class GmaDiariesApplication {
 		return model;
 	}
 	
+	@RequestMapping("/user")
+	public Principal user(Principal user) {
+		return user;
+	}
+	
+	@Configuration
+	@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+	protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.httpBasic()
+			.and().authorizeRequests()
+			.antMatchers("/index.html", "/home.html", "/login.html", "/").permitAll()
+			.anyRequest().authenticated().and()
+			.logout()
+			.and()
+			.addFilterAfter(new CsrfHeaderFilter(), CsrfFilter.class)
+			.csrf().csrfTokenRepository(csrfTokenRepository())
+			;
+		}
+		
+		/**TODO
+		 * Security will eventually be handled by 
+		 * LoadBalancer Sticky Sessions
+		 * or AWS ElasticCache
+		 * These services should be used to route requests
+		 * to the same JVM
+		 * Also, Spring Session uses Redis, as does ElastiCache
+		 */
+		private CsrfTokenRepository csrfTokenRepository() {
+			HttpSessionCsrfTokenRepository repository = 
+					new HttpSessionCsrfTokenRepository();
+			repository.setHeaderName("X-XSRF-TOKEN");
+			return repository;
+		}
+	}
+	
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(GmaDiariesApplication.class, args);
 	}
 }
+
+
